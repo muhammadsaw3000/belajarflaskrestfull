8eedaa9b5741f6b92b04f792f46524d5c5774d8c
AGUS AWALUDIN <aginthea@gmail.com>
2020-11-11 17:04:38 +0700

    mengubah msg apps

--- testflask.py
+++ testflask.py
@@ -17,7 +17,7 @@ class ContohResource(Resource):
         umur = request.form["umur"]
         identitas["nama"] = nama
         identitas["umur"] = umur
-        response = {"msg":"Data berhasil dimasukan"}
+        response = {"msg":"Data berhasil dimasukan bossku"}
         return response
     
 api.add_resource(ContohResource, "/api", methods=["GET", "POST"])
