9ba3527d4995f4596f98d01ffdc1e75d44c35f48
AGUS AWALUDIN <aginthea@gmail.com>
2020-11-16 10:41:11 +0700

    menambahkan resource

---
+++ insert.py
@@ -0,0 +1,37 @@
+import psycopg2
+from config import config
+
+
+def insert_salary(KODE_KANTOR,NAMA_KANTOR,ID_DOKUMEN,KETERANGAN,CENTANG,NAMA_BANK,NO_REKENING,ATAS_NAMA,NOM_TRANSFER,TGL_UBAH):
+    """ insert a new vendor into the vendors table """
+    sql = """INSERT INTO public."SALARY"("KODE_KANTOR","NAMA_KANTOR","ID_DOKUMEN","KETERANGAN","CENTANG","NAMA_BANK","NO_REKENING","ATAS_NAMA","NOM_TRANSFER","TGL_UBAH")
+             VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) RETURNING "SALARY_ID";"""
+    conn = None
+    salary_id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        #cur.execute(sql, ("43A","DEPUTI DIREKTUR BIDANG PENGEMBANGAN TEKNOLOGI INFORMASI","20101400655367","PEMBAYARAN GAJI NASIONAL BULAN OKTOBER 2020.",5,"MANDIRI","0700009726915","AGTRIA PRILIKA HERNIATY",10302093,"14/10/2020 18:02:13"))
+        cur.execute(sql, (KODE_KANTOR,NAMA_KANTOR,ID_DOKUMEN,KETERANGAN,CENTANG,NAMA_BANK,NO_REKENING,ATAS_NAMA,NOM_TRANSFER,TGL_UBAH))
+        # get the generated id back
+        salary_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return salary_id
+
+
+if __name__ == '__main__':
+    insert_salary("43A","DEPUTI DIREKTUR BIDANG PENGEMBANGAN TEKNOLOGI INFORMASI","20101400655367","PEMBAYARAN GAJI NASIONAL BULAN OKTOBER 2020.",5,"MANDIRI","0700009726915","AGTRIA PRILIKA HERNIATY",10302093,"14/10/2020 18:02:13")
--- testflask.py
+++ testflask.py
@@ -1,23 +1,45 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
+from insert import insert
 
 app = Flask(__name__)
 api = Api(app)
 CORS(app)
-identitas = {}
+SALARY = {}
+SALARY_ID = None
 
 class ContohResource(Resource):
     def get(self):
         #response = { "msg":"Hallo dunia, ini app restfull pertamaku"}
-        return identitas
+        return SALARY
     
     def post(self):
-        nama = request.form["nama"]
-        umur = request.form["umur"]
-        identitas["nama"] = nama
-        identitas["umur"] = umur
-        response = {"msg":"Data berhasil dimasukan bossku"}
+        KODE_KANTOR = request.form["KODE_KANTOR"]
+        NAMA_KANTOR = request.form["NAMA_KANTOR"]
+        ID_DOKUMEN = request.form["ID_DOKUMEN"]
+        KETERANGAN = request.form["KETERANGAN"]
+        CENTANG = request.form["CENTANG"]
+        NAMA_BANK = request.form["NAMA_BANK"]
+        NO_REKENING = request.form["NO_REKENING"]
+        ATAS_NAMA = request.form["ATAS_NAMA"]
+        NOM_TRANSFER = request.form["NOM_TRANSFER"]
+        TGL_UBAH = request.form["TGL_UBAH"]
+        
+        SALARY_ID = insert_salary(KODE_KANTOR,NAMA_KANTOR,ID_DOKUMEN,KETERANGAN,CENTANG,NAMA_BANK,NO_REKENING,ATAS_NAMA,NOM_TRANSFER,TGL_UBAH)
+        
+        SALARY["KODE_KANTOR"] = KODE_KANTOR
+        SALARY["NAMA_KANTOR"] = NAMA_KANTOR
+        SALARY["ID_DOKUMEN"] = ID_DOKUMEN
+        SALARY["KETERANGAN"] = KETERANGAN
+        SALARY["CENTANG"] = CENTANG
+        SALARY["NAMA_BANK"] = NAMA_BANK
+        SALARY["NO_REKENING"] = NO_REKENING
+        SALARY["ATAS_NAMA"] = ATAS_NAMA
+        SALARY["NOM_TRANSFER"] = NOM_TRANSFER
+        SALARY["TGL_UBAH"] = TGL_UBAH
+        
+        response = {"msg":"Data berhasil disimpan. Salary ID = "+SALARY_ID}
         return response
     
 api.add_resource(ContohResource, "/api", methods=["GET", "POST"])
