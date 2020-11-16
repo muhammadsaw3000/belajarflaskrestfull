671bdbf0b159c4d7f38779623e0c99e7d60a0109
AGUS AWALUDIN <aginthea@gmail.com>
2020-11-16 15:47:37 +0700

    mengubah msg apps

--- selectsalary.py
+++ selectsalary.py
@@ -1,6 +1,8 @@
 import psycopg2
 from config import config
 
+row = {}
+
 def select_salary():
     """ query data from the salary table """
     conn = None
@@ -8,7 +10,9 @@ def select_salary():
         params = config()
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        cur.execute('SELECT "SALARY_ID", "KODE_KANTOR", "NAMA_KANTOR", "ID_DOKUMEN", "KETERANGAN", "CENTANG", "NAMA_BANK", "NO_REKENING", "ATAS_NAMA", "NOM_TRANSFER", "TGL_UBAH" FROM public."SALARY" ORDER BY "KODE_KANTOR"')
+        sql = 'SELECT "SALARY_ID", "KODE_KANTOR", "NAMA_KANTOR", "ID_DOKUMEN", "KETERANGAN", "CENTANG", "NAMA_BANK", "NO_REKENING", "ATAS_NAMA", "NOM_TRANSFER", "TGL_UBAH" FROM public."SALARY" ORDER BY "KODE_KANTOR"'
+        cur.execute(sql)
+        print(sql)
         print("The number of parts: ", cur.rowcount)
         row = cur.fetchall()
         
@@ -18,13 +22,14 @@ def select_salary():
             #row = cur.fetchone()
         cur.close()
         print(row)
-        return row
+        
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
         if conn is not None:
             conn.close()
-            return {"msg":"Data tidak ditemukan."}
+    
+    return row
         
 #if __name__ == '__main__':
     #select_salary()
